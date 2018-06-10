2a1ae85796ee59476e85da4230927590beafbfef
Rodolfo Viola <rodolfoviolac@gmail.com>
2018-06-10 01:58:39 -0300

    fix predict

--- helpers/mlResources.js
+++ helpers/mlResources.js
@@ -5,53 +5,33 @@ var ml = require('machine_learning');
 let res = [];
 let dat = []
 let i, pluviometric, humidity, cloudness, pressure, ph, temperature, luminosity, forecastId, weatherId, soilMoisture, soilT0, soilT10;
-let quantidadeAmostra = 2000;
+let quantidadeAmostra = 800;
 
 for(i=0; i<quantidadeAmostra; i++) {
-  pluviometric =  Math.round(getRandomArbitrary(50,100));
+  pluviometric =  Math.round(getRandomArbitrary(100,150));
   humidity =      Math.round(getRandomArbitrary(50,100))
   cloudness =     Math.round(getRandomArbitrary(0,30));
   pressure =      Math.round(getRandomArbitrary(950,1050));
   luminosity =    Math.round(getRandomArbitrary(0,8));
-  ph =            Math.round(getRandomArbitrary(6,9));
-  temperature =   Math.round(getRandomArbitrary(15,27));
-  forecastId =    Math.round(getRandomArbitrary(300,530));
-  weatherId =     Math.round(getRandomArbitrary(300,530));
-  soilMoisture =  Math.round(getRandomArbitrary(10,30));
-  soilT0 =        Math.round(getRandomArbitrary(18,37));
-  soilT10 =       Math.round(getRandomArbitrary(15,35));
-
-  dat.push([pluviometric, humidity, cloudness, pressure, luminosity, ph, temperature, forecastId, weatherId, soilMoisture, soilT0, soilT10])
-  res.push(Math.round(getRandomArbitrary(65,85)));
-}
-
-for(i=0; i<quantidadeAmostra; i++) {
-  pluviometric =  Math.round(getRandomArbitrary(100,500));
-  humidity =      Math.round(getRandomArbitrary(0,50));
-  cloudness =     Math.round(getRandomArbitrary(50,100));
-  pressure =      Math.round(getRandomArbitrary(950,1050));
-  luminosity =    Math.round(getRandomArbitrary(0,8));
-  ph =            Math.round(getRandomArbitrary(9,14));
-  temperature =   Math.round(getRandomArbitrary(15,27));
-
+  ph =            Math.round(getRandomArbitrary(5,8));
+  temperature =   Math.round(getRandomArbitrary(15,34));
   forecastId =    Math.round(getRandomArbitrary(300,830));
   weatherId =     Math.round(getRandomArbitrary(300,830));
   soilMoisture =  Math.round(getRandomArbitrary(10,30));
-  soilT0 =        Math.round(getRandomArbitrary(18,32));
-  soilT10 =       Math.round(getRandomArbitrary(16,28));
-
+  soilT0 =        Math.round(getRandomArbitrary(18,37));
+  soilT10 =       Math.round(getRandomArbitrary(15,35));
   dat.push([pluviometric, humidity, cloudness, pressure, luminosity, ph, temperature, forecastId, weatherId, soilMoisture, soilT0, soilT10])
-  res.push(Math.round(getRandomArbitrary(80,100)));
+  res.push(Math.round(getRandomArbitrary(50,100)));
 }
 
 for(i=0; i<quantidadeAmostra; i++) {
-  pluviometric =  Math.round(getRandomArbitrary(50,100));
-  humidity =      Math.round(getRandomArbitrary(80,100));
-  cloudness =     Math.round(getRandomArbitrary(30,100));
-  pressure =      Math.round(getRandomArbitrary(950,1050));
-  luminosity =    Math.round(getRandomArbitrary(8,24));
-  ph =            Math.round(getRandomArbitrary(6,9));
-  temperature =   Math.round(getRandomArbitrary(39,45));
+  pluviometric =  Math.round(getRandomArbitrary(0,99));
+  humidity =      Math.round(getRandomArbitrary(0,50));
+  cloudness =     Math.round(getRandomArbitrary(0,30));
+  pressure =      Math.round(getRandomArbitrary(0,948));
+  luminosity =    Math.round(getRandomArbitrary(9,24));
+  ph =            Math.round(getRandomArbitrary(8,14));
+  temperature =   Math.round(getRandomArbitrary(35,100));
   forecastId =    Math.round(getRandomArbitrary(200,240));
   weatherId =     Math.round(getRandomArbitrary(200,240));
   soilMoisture =  Math.round(getRandomArbitrary(30,100));
@@ -62,23 +42,6 @@ for(i=0; i<quantidadeAmostra; i++) {
   res.push(Math.round(getRandomArbitrary(10,30)));
 }
 
-for(i=0; i<quantidadeAmostra; i++) {
-  pluviometric =  Math.round(getRandomArbitrary(50,100));
-  humidity =      Math.round(getRandomArbitrary(80,100));
-  cloudness =     Math.round(getRandomArbitrary(50,100));
-  pressure =      Math.round(getRandomArbitrary(950,1050));
-  luminosity =    Math.round(getRandomArbitrary(8,24));
-  ph =            Math.round(getRandomArbitrary(0,6));
-  temperature =   Math.round(getRandomArbitrary(-200,15));
-  forecastId =    Math.round(getRandomArbitrary(200,240));
-  weatherId =     Math.round(getRandomArbitrary(200,240));
-  soilMoisture =  Math.round(getRandomArbitrary(30,100));
-  soilT0 =        Math.round(getRandomArbitrary(37,100));
-  soilT10 =       Math.round(getRandomArbitrary(37,100));
-
-  dat.push([pluviometric, humidity, cloudness, pressure,luminosity, ph, temperature, forecastId, weatherId, soilMoisture, soilT0, soilT10])
-  res.push(Math.round(getRandomArbitrary(10,30)));
-}
 
 
 function getRandomArbitrary(min, max) {
--- routes/predict.js
+++ routes/predict.js
@@ -4,22 +4,19 @@ const predict = require('../helpers/mlResources')
 
 //Previsao Clima, Tempo Atual, Temperatura, PH, Luminosidade, Umidade, Pluviométrico
 module.exports = (req, res) => {
-  if(
-      !req.body.weatherId ||
-      !req.body.forecastId ||
-      !req.body.soilMoisture ||
-      !req.body.soilT0 ||
-      !req.body.soilT10 ||
-      !req.body.temperature ||
-      !req.body.ph ||
-      !req.body.luminosity ||
-      !req.body.humidity ||
-      !req.body.pluviometric ||
-      !req.body.cloudness ||
-      !req.body.pressure
-  ){
-    res.status(400).send('Error')
-  }
+   // if(req.body.weatherId > 200 &&
+   //   req.body.forecastId  > 200 &&
+   //   req.body.soilMoisture > 0 &&
+   //   req.body.soilT0 > 0 &&
+   //   req.body.soilT10 > 0 &&
+   //   req.body.temperature > 0 &&
+   //   req.body.ph > 0 &&
+   //   req.body.luminosity > 0 &&
+   //   req.body.humidity > 0 &&
+   //   req.body.pluviometric > 0 &&
+   //   req.body.cloudness > 0 &&
+   //   req.body.pressure> 0) res.status(400).send('Error')
+
   let result = predict.classify([
     req.body.weatherId,
     req.body.forecastId,
