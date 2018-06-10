90783a2208869a582635230ebe48e6e800f4683c
Rodolfo Viola <rodolfoviolac@gmail.com>
2018-06-10 01:24:51 -0300

    Fix predict route

--- routes/predict.js
+++ routes/predict.js
@@ -4,24 +4,36 @@ const predict = require('../helpers/mlResources')
 
 //Previsao Clima, Tempo Atual, Temperatura, PH, Luminosidade, Umidade, Pluviométrico
 module.exports = (req, res) => {
-  if(!req.body.previsao ||
-    !req.body.climaAtual ||
-    !req.body.temperatura ||
-    !req.body.ph ||
-    !req.body.luminosidade ||
-    !req.body.umidade ||
-    !req.body.pluviometrico){
+  if(
+      !req.body.weatherId ||
+      !req.body.forecastId ||
+      !req.body.soilMoisture ||
+      !req.body.soilT0 ||
+      !req.body.soilT10 ||
+      !req.body.temperature ||
+      !req.body.ph ||
+      !req.body.luminosity ||
+      !req.body.humidity ||
+      !req.body.pluviometric ||
+      !req.body.cloudness ||
+      !req.body.pressure
+  ){
     res.status(400).send('Error')
   }
-  let result = predict.classify([req.body.previsao, req.body.climaAtual, req.body.temperatura,req.body.ph,req.body.luminosidade,req.body.umidade,req.body.pluviometrico])
+  let result = predict.classify([
+    req.body.weatherId,
+    req.body.forecastId,
+    req.body.soilMoisture,
+    req.body.soilT0,
+    req.body.soilT10,
+    req.body.temperature,
+    req.body.ph,
+    req.body.luminosity,
+    req.body.humidity,
+    req.body.pluviometric,
+    req.body.cloudness,
+    req.body.pressure])
   let porcent = Object.keys(result)[0];
   console.log(result)
   res.status(200).send({resultado: porcent})
 };
-
-
-// let sensors = {}
-// let uv = await Uv.findOne().sort({createdAt:-1}).exec();
-// let weather = await Weather.findOne().sort({createdAt:-1}).exec();
-// let forecast = await Forecast.findOne().sort({createdAt:-1}).exec();
-// let Soil = await Forecast.findOne().sort({createdAt:-1}).exec();
